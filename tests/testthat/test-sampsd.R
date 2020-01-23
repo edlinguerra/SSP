c9edc2757d37a945ec655302b0e3c95e322995cc
Edlin Jose Guerra Castro <edlinguerra@gmail.com>
2020-01-23 10:28:43 -0600

    reduccion de argumentos

--- tests/testthat/test-sampsd.R
+++ tests/testthat/test-sampsd.R
@@ -9,11 +9,11 @@ test_that("sampsd generates a matrix including all estimated MultSE for each sim
   x<-epibionts[1:8, 2:length(epibionts)]
   par.epi<-assempar(x, "P/A")
   cases<-3
-  n<-30
-  sites<-1
+  N<- n <-30
+  sites <- m <-1
   k<-10
-  sim.epi<-simdata(Par = par.epi, cases = cases, n = n, sites = sites)
-  results<-sampsd(sim.epi, par.epi, transformation = "P/A", method = "jaccard", multi.site = FALSE, n = n, p.n= n, sites = sites, p.s = sites, k= 10)
+  sim.epi<-simdata(Par = par.epi, cases = cases, N = N, sites = sites)
+  results<-sampsd(sim.epi, par.epi, transformation = "P/A", method = "jaccard", n = N, m = sites, k= 10)
   expect_is(results, "matrix")
   expect_equal(nrow(results), cases * (n-1) * k)
 
@@ -22,11 +22,11 @@ test_that("sampsd generates a matrix including all estimated MultSE for each sim
   x<-epibionts[1:32, 2:length(epibionts)]
   par.epi<-assempar(x, "P/A")
   cases<-3
-  n<-30
-  sites<-3
+  N<- n <-30
+  sites <- m <-3
   k<-10
-  sim.epi<-simdata(Par = par.epi, cases = cases, n = n, sites = sites)
-  results<-sampsd(sim.epi, par.epi, transformation = "P/A", method = "jaccard", multi.site = TRUE, n = n, p.n= n, sites = sites, p.s = sites, k= 10)
+  sim.epi<-simdata(Par = par.epi, cases = cases, N = N, sites = sites)
+  results<-sampsd(sim.epi, par.epi, transformation = "P/A", method = "jaccard", n = N, m = sites, k= 10)
   expect_is(results, "matrix")
   expect_equal(nrow(results), cases * (n-1) * (sites-1) * k)
 })
