fefd112bbb8aaca35fb0eb370f30f0a8c12469d9
Edlin Jose Guerra Castro <edlinguerra@gmail.com>
2020-02-01 08:27:28 -0600

    corrección a posicion de derivada en la tabla, agregué suma acumulada de mejoría

--- R/summary_ssp.R
+++ R/summary_ssp.R
@@ -2,7 +2,7 @@
 #'@importFrom stats quantile
 #'@importFrom stats aggregate
 #'@export
-summary_ssp<- function(results, multi.site = TRUE) {
+summary_ssp<- function(results, multi.site) {
   lower <- function(x) {
     quantile(x, 0.025)
   }
@@ -41,19 +41,24 @@ summary_ssp<- function(results, multi.site = TRUE) {
     max.mse.sites<- max(xx[xx$sv=="sites", 2])
     xx$rel<-c((xx[xx$sv=="sites", 2]/max.mse.sites)*100, (xx[xx$sv=="samples", 2]/max.mse.samples)*100)
     xx$der<-c(rep(NA, nrow(xx)))
-    mse.sites<-xx[xx$sv=="sites",c(1,6)]
-    mse.residual<-xx[xx$sv=="samples",c(1,6)]
+    mse.sites<-xx[xx$sv=="sites",c(1,6,7)]
+    mse.residual<-xx[xx$sv=="samples",c(1,6,7)]
 
-    for (i in 1:nrow(mse.sites)){
-      mse.sites$der[i]<-(mse.sites$rel[i+1]-mse.sites$rel[i])/(mse.sites$samples[i]+1-mse.sites$samples[i])
+    for (i in 1:(nrow(mse.sites)-1)){
+      mse.sites$der[i+1]<-(mse.sites$rel[i]-mse.sites$rel[i+1])/(mse.sites$samples[i+1]-mse.sites$samples[i])
     }
+    mse.sites$cum<-c(NA,cumsum(mse.sites$der[2:nrow(mse.sites)]))
 
-    for (i in 1:nrow(mse.residual)){
-      mse.residual$der[i]<-(mse.residual$rel[i+1]-mse.residual$rel[i])/(mse.residual$samples[i]+1-mse.residual$samples[i])
+    for (i in 1:(nrow(mse.residual)-1)){
+      mse.residual$der[i+1]<-(mse.residual$rel[i]-mse.residual$rel[i+1])/(mse.residual$samples[i+1]-mse.residual$samples[i])
     }
+    mse.residual$cum<-c(NA,cumsum(mse.residual$der[2:nrow(mse.residual)]))
 
     xx$der<-abs(c(mse.sites$der, mse.residual$der))
     xx$der<-round(xx$der, 3)
+
+    xx$cum<-c(mse.sites$cum, mse.residual$cum)
+    xx$cum<-round(xx$cum, 0)
     return(xx)
   }
   if (multi.site == FALSE) {
@@ -73,9 +78,11 @@ summary_ssp<- function(results, multi.site = TRUE) {
     xx$der<-c(rep(NA, nrow(xx)))
 
     for (i in 1:(nrow(xx)-1)){
-     xx$der[i]<-(xx$rel[i+1]-xx$rel[i])/(xx$samples[i]+1-xx$samples[i])
+     xx$der[i+1]<-(xx$rel[i]-xx$rel[i+1])/(xx$samples[i+1]-xx$samples[i])
     }
     xx$der<-abs(round(xx$der, 3))
+    cum<-c(NA,cumsum(xx$der[2:nrow(xx)]))
+    xx$cum<-round(cum, 0)
     return(xx)
   }
 }
